d62660d5747b02daf6d6d6aa31043a3a514675ec
Antsukemmytech <antsukemmytech@gmail.com>
2023-06-22 00:56:13 -0700

    dubugging testing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,20 +6,15 @@
 */
 
 int main(void)
+
 {
-<<<<<<< HEAD
-        int i;
 
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
-=======
 	int i;
 
 	i = 98;
+
 	positive_or_negative(i);
 
-	return (0);
->>>>>>> 77cd4013b4c0e5d0a1d46fb18ca88bcbb70f7af5
+return (0);
+
 }
