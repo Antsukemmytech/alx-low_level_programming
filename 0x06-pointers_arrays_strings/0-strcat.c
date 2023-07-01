bfab25903a7a750b4e5caab5ca00c300ec92737a
Antsukemmytech <antsukemmytech@gmail.com>
2023-06-30 23:29:53 -0700

    changes added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,11 +16,15 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		dest[i] = src[j];
 		i++;
-		j++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+			i++;
+			j++;
 	}
 	dest[i] = '\0';
 	return (dest);
 }
-
