085ff0e6785f6e8c37116eb913ea892d0975c463
Antsukemmytech <antsukemmytech@gmail.com>
2023-07-18 00:13:13 -0700

    revisited codes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char letter;
+	for(letter = 'a'; a <= 'z'; letter++)
+	{
+		print_alphabet("%c\n", letter);
+	}
+	return (0);
+}
